646edb3ab213297fa139b8713900355af3e1f1fe
Rachel Moser <moser.rachel@gmail.com>
2021-03-12 17:48:03 -0600

    Add and set-up gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -73,4 +75,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Add Rack::LiveReload to the bottom of the middleware stack with the default options:
+  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 end
